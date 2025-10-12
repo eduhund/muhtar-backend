9a8bc73fe0385a55c45c64367386bbb2e1ceb3f0
Roman Nebel <r@nebel.im>
2025-10-12 17:40:46 +0200

    feat: remove unused UUID utility functions to streamline codebase

--- src/utils/id.ts
+++
@@ -1,7 +0,0 @@
-import { parse as uuidParse } from "uuid";
-import { Binary } from "mongodb";
-
-export function getBinaryUUID(uuid: string) {
-  const uuidBuffer = Buffer.from(uuidParse(uuid));
-  return new Binary(uuidBuffer, Binary.SUBTYPE_UUID);
-}
