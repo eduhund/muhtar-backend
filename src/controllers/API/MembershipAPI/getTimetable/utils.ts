cad9b87a23619ecc402ff3f84777bcae038e4bda
Roman Nebel <r@nebel.im>
2025-10-15 12:01:00 +0200

    refactor: enhance date validation logic in validateGetTimeParams function

--- src/controllers/API/MembershipAPI/getTimetable/utils.ts
+++ src/controllers/API/MembershipAPI/getTimetable/utils.ts
@@ -1,12 +1,32 @@
-import { dateOnlyIsoString } from "../../../../utils/date";
 import { InvalidParamsError } from "../../../../utils/Rejection";
 
+function checkDateString(dateStr: string) {
+  if (typeof dateStr !== "string")
+    throw new InvalidParamsError("'date' must be a string");
+
+  const isoDateRegex = /^\d{4}-\d{2}-\d{2}$/;
+  if (!isoDateRegex.test(dateStr)) {
+    throw new InvalidParamsError("'date' must be in ISO format YYYY-MM-DD");
+  }
+}
+
 export default function validateGetTimeParams(req: any, res: any, next: any) {
-  const { projectId, membershipId, date, from, to, withDeleted } = req.query;
-  if (!date && !(from && to)) {
+  const { projectId, membershipId, date, from, to, withArchived } = req.query;
+  if (projectId && typeof projectId !== "string") {
+    throw new InvalidParamsError("'projectId' must be a string");
+  }
+  if (membershipId && typeof membershipId !== "string") {
+    throw new InvalidParamsError("'membershipId' must be a string");
+  }
+  date && checkDateString(date);
+  from && checkDateString(from);
+  to && checkDateString(to);
+
+  if (withArchived && withArchived !== "true" && withArchived !== "false") {
     throw new InvalidParamsError(
-      "Either 'date' or both 'from' and 'to' parameters must be provided"
+      "'withArchived' must be boolean-type string: 'true' or 'false'"
     );
   }
+
   return next();
 }
