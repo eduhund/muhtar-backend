63aca87d0fb9f3bc028ad4cdc8263479db65de94
Roman Nebel <r@nebel.im>
2025-10-23 20:36:06 +0200

    refactor: enhance getMe function by adding project service and restructuring data assignment

--- src/flows/User/getMe.ts
+++ src/flows/User/getMe.ts
@@ -1,6 +1,7 @@
 import Membership from "../../models/Membership";
+import Project from "../../models/Project";
 import User from "../../models/User";
-import { membershipService, teamService } from "../../services";
+import { membershipService, projectService, teamService } from "../../services";
 
 export default async function getMe(actorUser: User) {
   const userId = actorUser.getId();
@@ -12,12 +13,31 @@ export default async function getMe(actorUser: User) {
     ? await teamService.getTeamById(activeMembership.teamId)
     : null;
 
-  const data = {
+  const data: any = {
     ...actorUser.toJSON(),
     memberships: memberships.map((m: Membership) => m.toJSON()),
-    activeMembership: activeMembership ? activeMembership.toJSON() : null,
-    activeTeam: activeTeam ? activeTeam.toJSON() : null,
   };
+
+  if (activeMembership) {
+    data.activeMembership = activeMembership.toJSON();
+  }
+
+  if (activeTeam) {
+    data.activeTeam = activeTeam.toJSON();
+
+    const teamMemberships = await membershipService.getMemberships({
+      teamId: activeTeam.getId(),
+    });
+    data.activeTeam.memberships = teamMemberships.map((m: Membership) =>
+      m.toJSON()
+    );
+
+    const teamProjects = await projectService.getProjects({
+      teamId: activeTeam.getId(),
+    });
+
+    data.activeTeam.projects = teamProjects.map((p: Project) => p.toJSON());
+  }
 
   delete data.activeMembershipId;
 
