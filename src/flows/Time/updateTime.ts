8c7173109862291b59037100092281d2f989ccb6
Roman Nebel <r@nebel.im>
2025-11-05 23:20:48 +0100

    fix: ensure date is correctly converted to ISO string in updateTime function

--- src/flows/Time/updateTime.ts
+++ src/flows/Time/updateTime.ts
@@ -90,7 +90,7 @@ export default async function updateTime(
       membershipId,
       projectId,
       taskId,
-      date: date ? dateOnlyIsoString(date) : undefined,
+      date: date ? dateOnlyIsoString(new Date(date)) : undefined,
       duration,
       comment,
     },
