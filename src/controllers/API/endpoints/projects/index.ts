3970112f62660ecc016de7b21d69bafe7d7a0ebb
Roman Nebel <r@nebel.im>
2025-11-12 11:41:25 +0100

    refactor: remove unused membership management endpoints from project API

--- src/controllers/API/endpoints/projects/index.ts
+++ src/controllers/API/endpoints/projects/index.ts
@@ -3,8 +3,6 @@ import create from "./create";
 import update from "./update";
 import archive from "./archive";
 import restore from "./restore";
-import addMemberships from "./addMemberships";
-import removeMembership from "./removeMembership";
 
 const router = Router();
 
@@ -12,7 +10,5 @@ router.post("/create", create);
 router.post("/:id/update", update);
 router.post("/:id/archive", archive);
 router.post("/:id/restore", restore);
-router.post("/:id/addMemberships", addMemberships);
-router.post("/:id/removeMembership", removeMembership);
 
 export default router;
