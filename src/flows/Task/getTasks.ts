ca792b8495cd3361031446d7bc9da94eb7e878e5
Roman Nebel <r@nebel.im>
2025-11-14 17:02:13 +0100

    feat: refactor getTasks to include project and membership details in task retrieval

--- src/flows/Task/getTasks.ts
+++ src/flows/Task/getTasks.ts
@@ -1,6 +1,12 @@
 import Membership from "../../models/Membership";
+import Project from "../../models/Project";
 import Task from "../../models/Task";
-import { taskService } from "../../services";
+import {
+  membershipService,
+  projectService,
+  taskService,
+  teamService,
+} from "../../services";
 
 type GetProjectsFilters = {
   projectId: string;
@@ -13,15 +19,25 @@ export default async function getTasks(
 ) {
   const { teamId } = actorMembership;
 
+  const projects = (await projectService.getProjectsByTeam(
+    teamId
+  )) as Project[];
+  const team = await teamService.getTeamById(teamId);
+  const memberships = await membershipService.getMembershipsByTeam(teamId);
+
   const taskList = await taskService.getTasks({
     teamId,
     projectId,
     assignedMembershipId,
   });
 
-  const extentedTaskList = taskList.map((task: Task) =>
-    taskService.getRichTask({ task })
-  );
+  const extentedTaskList = taskList.map((task: Task) => {
+    const membership = memberships.find(
+      (m: Membership) => m.getId() === task.assignedMembershipId
+    );
+    const project = projects.find((p: Project) => p.getId() === task.projectId);
+    return taskService.getRichTask({ task, membership, project, team });
+  });
 
   return extentedTaskList;
 }
