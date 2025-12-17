3966e79f3194446d2de77d2167d35f0c0124baa4
Roman Nebel <r@nebel.im>
2025-12-17 14:00:03 +0100

    refactor: remove unused teamId parameter from createProjectFlow call

--- src/controllers/API/endpoints/projects/create.ts
+++ src/controllers/API/endpoints/projects/create.ts
@@ -3,11 +3,8 @@ import { createProjectFlow } from "../../../../flows";
 export default async function create(req: any, res: any, next: any) {
   try {
     const { currentUser } = req.data;
-    const { name, description, teamId } = req.body;
-    const data = await createProjectFlow(
-      { name, description, teamId },
-      currentUser
-    );
+    const { name, description } = req.body;
+    const data = await createProjectFlow({ name, description }, currentUser);
     return next({ data });
   } catch (e) {
     return next(e);
