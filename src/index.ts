046fd0d0c6921a88a4bcc383d747dc28f82b0281
Roman Nebel <r@nebel.im>
2026-02-05 14:34:22 +0100

    refactor: remove commented-out code and unused imports from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import log from "./utils/log";
 import expressServer from "./connectors/ExpressJS";
 import * as mongo from "./connectors/MongoDB";
-import { sendDailyTimeTrackReminder } from "./scripts";
-import { setWorkdays } from "./utils/isDayOff";
+//import { sendDailyTimeTrackReminder } from "./scripts";
+//import { setWorkdays } from "./utils/isDayOff";
 
 (async () => {
   try {
@@ -18,57 +18,3 @@ import { setWorkdays } from "./utils/isDayOff";
     process.exit(1);
   }
 })();
-
-/*
-app.use("/api", api);
-
-api.get("/timetable", async (req: Request, res: Response) => {
-  const data = await getTimetable(req.query);
-  res.send(data);
-});
-
-api.post("/timetable/addTime", async (req: Request, res: Response) => {
-  console.log("Adding time entry:", req.body);
-  await setNewTime(req.body);
-  res.sendStatus(200);
-});
-
-api.get("/users", async (req: Request, res: Response) => {
-  const users = await getUsers(req.query);
-  res.status(200).send(users);
-});
-
-api.get("/users/:userId", (req: Request, res: Response) => {
-  const { userId } = req.params;
-  console.log(userId);
-  res.sendStatus(200);
-});
-
-api.get("/projects", (req: Request, res: Response) => {
-  console.log(req.query);
-  res.sendStatus(200);
-});
-
-api.get("/projects/:projectId", (req: Request, res: Response) => {
-  const { projectId } = req.params;
-  console.log(projectId);
-  res.sendStatus(200);
-});
-
-app.get("*", (req: Request, res: Response) => {
-  const filePath = path.join(__dirname, frontendPath, "index.html");
-
-  if (fs.existsSync(filePath)) {
-    res.sendFile(filePath);
-  } else {
-    console.warn("File doesn't exist: ", filePath);
-    res.status(404).send("Page not found");
-  }
-});
-
-startDB();
-
-app.listen(PORT, () => {
-  console.log("Server is listening port 3000");
-});
-*/
